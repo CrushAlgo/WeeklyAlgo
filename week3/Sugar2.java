ee8e2f8c52cba81d9525a112b2a880fd4b08b2b3
parkyounghwan <dudrnxps1@gmail.com>
2019-03-04 00:27:19 +0900

    week3 update

--- week3/Sugar2.java
+++ week3/Sugar2.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class Sugar2 {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int N = scan.nextInt();
+    scan.nextLine();
+
+    System.out.println(solution(N));
+  }
+
+  public static int solution(int N) {
+    int result = 0;
+
+    int three = 0;
+
+    int five = (N / 5);
+    int left = (N % 5);
+
+    if ((N % 5) == 0) return five;
+    else {
+      while (five >= 0) {
+        if ((left % 3) == 0) {
+          three = (left / 3);
+          break;
+        }
+
+        five--;
+        left += 5;
+      }
+
+      result = (five < 0) ? -1 : five + three;
+    }
+
+    return result;
+  }
 }
