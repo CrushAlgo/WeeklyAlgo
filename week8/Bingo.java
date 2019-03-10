023ae716a4f17a5875a0830dceb7b99df0dec875
parkyounghwan <dudrnxps1@gmail.com>
2019-03-10 18:55:35 +0900

    week8, Bingo & BlackJack & Move & README upload

---
+++ week8/Bingo.java
@@ -0,0 +1,95 @@
+package week8;
+
+import java.util.Scanner;
+
+/**
+ * 같은 행, 같은 열이 지워지면 빙고, 대각선은 2개.
+ */
+public class Bingo {
+
+  public static int SIZE = 5;
+
+  public static void main(String[] args) {
+    String[][] bingo = new String[SIZE][SIZE];
+    String[][] bingoNum = new String[SIZE][SIZE];
+
+    try (Scanner scan = new Scanner(System.in)) {
+      for (int i = 0; i < (SIZE + SIZE); i++) {
+
+        if (i < 5) bingo[i] = scan.nextLine().split(" ");
+        else bingoNum[i - 5] = scan.nextLine().split(" ");
+      }
+    }
+
+    System.out.println(solution(bingo, bingoNum));
+  }
+
+  public static int solution(String[][] bingo, String[][] bingoNum) {
+    int result = 0;
+
+    int[] row = new int[SIZE];      //0,1,2,3,4
+    int[] column = new int[SIZE];   //0,1,2,3,4
+
+    //대각선 (0,0) (1,1) ... (4,4), (0,4) (1,3) ... (4,0), 인덱스가 같던지 합이 4가 되던지
+    int leftDiagonal = 0;
+    int rightDiagonal = 0;
+
+    int bingoCnt = 0;
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        int num = Integer.parseInt(bingoNum[i][j]);
+        int[] indexArr = bingoIndex(num, bingo);
+
+        row[indexArr[0]]++;
+        column[indexArr[1]]++;
+
+        //대각선을 잘못 설정.
+        //왼쪽 위에서 부터 우하향 하는 대각선은, 2차원 배열 상의 인덱스 가 같다.
+        if (indexArr[0] == indexArr[1]) leftDiagonal++;
+        //오른쪽 위에서 부터 좌하향 하는 대각선은, 인덱스의 합이 '4'이다.
+        if ((indexArr[0] + indexArr[1]) == 4) rightDiagonal++;
+
+        if (row[indexArr[0]] == 5) bingoCnt++;
+        if (column[indexArr[1]] == 5) bingoCnt++;
+
+        if (leftDiagonal == 5) {
+          bingoCnt++;
+          leftDiagonal++; //계속 카운트 되는 걸 방지
+        }
+        if (rightDiagonal == 5) {
+          bingoCnt++;
+          rightDiagonal++;
+        }
+
+        result++;
+        if (bingoCnt >= 3) {
+          return result;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  public static int[] bingoIndex(int num, String[][] bingo) {
+    int[] result = new int[2];
+
+    boolean flag = false;
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        int bNum = Integer.parseInt(bingo[i][j]);
+        if (bNum == num) {
+          result[0] = i;
+          result[1] = j;
+          flag = true;
+          break;
+        } else continue;
+      }
+      if (flag) break;
+    }
+
+    return result;
+  }
+
+
+}
---
+++ week8/Move.java
@@ -0,0 +1,76 @@
+package week8;
+
+import java.util.*;
+
+public class Move {
+
+  public static void main(String[] args) {
+    int N;
+    int[] xArr, yArr;
+
+    try (Scanner scan = new Scanner(System.in)) {
+      N = scan.nextInt();
+      scan.nextLine();
+
+      String[] xInput = scan.nextLine().split(" ");
+      String[] yInput = scan.nextLine().split(" ");
+
+      xArr = new int[N];
+      yArr = new int[N];
+      for (int i = 0; i < N; i++) {
+        xArr[i] = Integer.parseInt(xInput[i]);
+        yArr[i] = Integer.parseInt(yInput[i]);
+      }
+    }
+
+    System.out.println(solution(N, xArr, yArr));
+  }
+
+  public static int solution(int N, int[] xArr, int[] yArr) {
+    int result = 0;
+
+    /*
+    N = 4
+    (1 % 4) = 1, .. (3 % 4) = 3, (4 % 4) = 0
+    N으로 수를 나누면, 0 ~ (N-1) 사이의 수를 출력한다.
+    이를 이용해서 배열의 인덱스를 설정한다.
+     */
+    List<Integer> tempXList = new LinkedList<>();
+    List<Integer> tempYList = new LinkedList<>();
+
+    /*
+    곱, N*N 행렬 두개를 만들어서 곱 연산을 실행한다.
+     */
+    int[][] xMatrix = new int[N][N];
+    int[][] yMatrix = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      int count = 0;
+      int start = i;
+      while (count < N) {
+        int index = (start % N);
+        xMatrix[i][count] = xArr[index];
+        yMatrix[i][count] = yArr[index];
+
+        start++;
+        count++;
+      }
+    }
+
+    //곱
+    int S = 0;
+    for (int i = 0; i < N; i++) {           //고정
+      int[] temp = xMatrix[i];
+      for(int k = 0; k < N; k++) {          //행
+        for (int j = 0; j < N; j++) {       //열
+          S += (temp[j] * yMatrix[j][k]);
+        }
+        if (S > result) result = S;
+
+        S = 0;
+      }
+    }
+
+    return result;
+  }
+}
