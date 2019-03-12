f812a34d70d6340fe6ba6527ff258c14d3775987
parkyounghwan <dudrnxps1@gmail.com>
2019-03-12 17:23:53 +0900

    week8 README & 저항& 퇴사 update

--- week8/Resignation.java
+++ week8/Resignation.java
@@ -2,6 +2,9 @@ package week8;
 
 import java.util.Scanner;
 
+/*
+이차원 배열에 정보들을 표시해서 계산 한다.
+ */
 public class Resignation {
 
   public static void main(String[] args) {
@@ -21,7 +24,7 @@ public class Resignation {
       }
     }
 
-    solution(N, input);
+    System.out.println(solution(N, input));
   }
 
   public static int solution(int N, int[][] inputArr) {
@@ -30,13 +33,39 @@ public class Resignation {
     int[] period = inputArr[0];
     int[] price = inputArr[1];
 
-    for (int i = 0; i < N; i++) {
-      int start = period[i];
-      System.out.format("start: %d", i);
-      for (int j = start + i; j < N; j += period[j]) {
-        System.out.format(" %d", j);
+    int periodSum = 0;
+    int priceSum = 0;
+
+    for (int i = 0; i < N; i++) {   //고정
+      int start = period[i];  //인덱스
+      periodSum += start;
+      priceSum += price[i];
+
+      System.out.format("start: %d \n", i);
+
+      for (int k = start; k < N; k++) { //첫번째 날을 거친 후 인덱스 부터 계산
+        System.out.print(k + " : ");
+        for (int j = k + i; j < N; j += period[j]) {
+          System.out.print(j + " ");
+
+          periodSum += period[j];
+          priceSum += price[j];
+
+          if(periodSum >= N) {
+            if(periodSum > N ) priceSum -= price[j];
+
+            if(priceSum > result) result = priceSum;
+
+            System.out.print(" / " + result);
+
+            break;
+          }
+        }
+        priceSum = price[i];
+        periodSum = start;
+
+        System.out.println();
       }
-      System.out.println();
     }
 
     return result;
